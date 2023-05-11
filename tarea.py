485994680b0228eee122cef5e306da66f8f9aed2
Mirian Pico <miriancpico@gmail.com>
2023-05-11 14:12:52 -0400

    cambio

--- tarea.py
+++ tarea.py
@@ -1,15 +1,8 @@
-
-##Ejercicios Python
-#1. Para tributar un determinado impuesto se debe ser mayor de 16 años y tener unos ingresos iguales o superiores a 1000 € mensuales.#
- #Escribir un programa que pregunte al usuario su edad y sus ingresos mensuales y muestre por pantalla si el usuario tiene que tributar o no.#
-
-#2. Escribir un programa que pida al usuario una palabra y la muestre por pantalla 10 veces.#
-
-
-#3. Escribir un programa que pida al usuario un número entero positivo y muestre por pantalla la cuenta atrás desde ese número hasta cero separados por comas#
-
 #4. Escribir un programa que pida al usuario un número entero y muestre por pantalla si es un número primo o no.#
 
 #5. Escribir un programa que almacene en una lista los siguientes precios, 50, 75, 46, 22, 80, 65, 8, y muestre por pantalla el menor y el mayor de los precios.#
 
 #6. Escribir un programa que pida al usuario una palabra y muestre por pantalla si es un palíndromo#
+
+
+
