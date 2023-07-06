721359c7a253149f972d352f628f7ba8ce6bd995
Mirian Pico <miriancpico@gmail.com>
2023-07-06 18:10:10 -0400

    removido

--- tarea.py
+++
@@ -1,8 +0,0 @@
-#4. Escribir un programa que pida al usuario un número entero y muestre por pantalla si es un número primo o no.#
-
-#5. Escribir un programa que almacene en una lista los siguientes precios, 50, 75, 46, 22, 80, 65, 8, y muestre por pantalla el menor y el mayor de los precios.#
-
-#6. Escribir un programa que pida al usuario una palabra y muestre por pantalla si es un palíndromo#
-
-
-
